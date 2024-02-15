e99cb9ab0af1d8dcc4525886be40fea1defcbc17
Jahidul Islam <jahidul@pop-os.localdomain>
2024-02-15 16:10:17 +0600

    how to modular code in js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const circle = require('./circle')
 console.log(circle(3))
 
 const fs = require('fs')
-let text = fs.readFileSync('./files/demo.txt', 'utf-8',)
-console.log(text)
+let text = fs.readFileSync('./files/demo.txt', 'utf-8')
+let textDocumnets = "Hi . How are you"
+fs.writeFileSync('./files/writefile.txt', text)
