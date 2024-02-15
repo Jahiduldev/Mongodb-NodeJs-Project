bc1150ecf9fdba05ba48d9008de7744d4c4f81bb
Jahidul Islam <jahidul@pop-os.localdomain>
2024-02-15 15:48:28 +0600

    how to modular code in js

---
+++ circle.js
@@ -0,0 +1,15 @@
+const circle = r => r * r;
+const sum = (a, b) => a + b;
+
+const name = "jahidul islam"
+
+module.exports.circle = circle
+module.exports.sum = sum
+
+exports.name = name;
+
+console.log(module)
+
+
+
+
